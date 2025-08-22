4a1a5150c67e5080147023d60f103da867cbb8fe
Krishna <Krishnachaudhari0340@gmail.com>
2025-08-22 19:11:12 +0530

    Initial commit

---
+++ Begginer.java
@@ -0,0 +1,13 @@
+public class Begginer {
+
+  public static void main(String[] args) {
+    int a = 10;
+    int b = 20;
+    int c = a + b;
+    System.out.println(c);
+    String name = "my name is krishna";
+    System.out.println(name);
+    for (i= 1)
+
+    }
+}
